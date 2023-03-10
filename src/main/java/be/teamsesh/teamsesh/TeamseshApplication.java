faab9f6fe8387c7732789de1f87d3427b1b631b3
XNGLN <81490934+xngln24@users.noreply.github.com>
2023-03-10 21:05:28 +0100

    Setup

---
+++ src/main/java/be/teamsesh/teamsesh/TeamseshApplication.java
@@ -0,0 +1,14 @@
+package be.teamsesh.teamsesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamseshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamseshApplication.class, args);
+		//test
+	}
+
+}
---
+++ src/test/java/be/teamsesh/teamsesh/TeamseshApplicationTests.java
@@ -0,0 +1,13 @@
+package be.teamsesh.teamsesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamseshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
